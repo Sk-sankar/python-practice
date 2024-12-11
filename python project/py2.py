998749e41ec2a1dd826f83a593f52912e81a39c2
Sankar <sankarkarunakaran1109@gmail.com>
2024-12-11 16:01:05 +0530

    practicing python

--- python project/py2.py
+++ python project/py2.py
@@ -26,3 +26,33 @@ def rock_paper_scissors():
             print("You lose!")
 
 rock_paper_scissors()
+
+
+# from flask import Flask, render_template, request
+# import random
+
+# app = Flask(__name__)
+
+# @app.route("/", methods=["GET"])
+# def index():
+#     return render_template("index.html", computer_choice="", result="")
+
+# @app.route("/play", methods=["POST"])
+# def play():
+#     user_choice = request.form.get("choice")
+#     options = ["rock", "paper", "scissors"]
+#     computer_choice = random.choice(options)
+
+#     if user_choice == computer_choice:
+#         result = "It's a tie!"
+#     elif (user_choice == "rock" and computer_choice == "scissors") or \
+#          (user_choice == "paper" and computer_choice == "rock") or \
+#          (user_choice == "scissors" and computer_choice == "paper"):
+#         result = "You win!"
+#     else:
+#         result = "You lose!"
+
+#     return render_template("index.html", computer_choice=computer_choice, result=result)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
