db359d1defb2f43246986b224511f4584d941eea
Sankar <sankarkarunakaran1109@gmail.com>
2024-12-19 11:12:59 +0530

    file handling

---
+++ 18.12.24/add.py
@@ -0,0 +1,9 @@
+def add(n1,n2):
+    return(n1+n2)
+
+
+
+if __name__== "__main__" :
+    print(add(3,5))
+   
+    print(__name__)
--- 18.12.24/sum1.py
+++ 18.12.24/sum1.py
@@ -1,3 +1,20 @@
-file=open("textfile")
+# # file=open("textfile","w")
 
-print(file.readline())
+# # print(file.write("\n iam sankar"))
+
+# # file.close()
+
+# # f=open("textfile","a")
+
+# # print(f.write(" \n khk"))
+# # print(f.read())
+
+
+# f=open("textfile","r+")
+
+# print(f.readline())
+
+from add import add 
+
+print(add(2,3))
+print(__name__)
