e82121aa1ef7827e8c3f6d8bc7419b3af4be4468
Sankar <sankarkarunakaran1109@gmail.com>
2024-12-20 11:33:48 +0530

    python practice

---
+++ 18.12.24/file.py
@@ -0,0 +1,13 @@
+
+# for i in range(1, 5):
+#     for j in range(i):
+#          print(i, end=' ')
+#     print()
+
+a = range(1,10)
+
+
+
+# testing the type of a
+print ("The return type of range() is : ")
+print (a)
---
+++ 18.12.24/sum2.py
@@ -0,0 +1,22 @@
+# def contains_even_number(l):
+#     a = []  # Properly indented this line
+#     for ele in l:
+#         if ele % 2 == 0:
+#             a.append(ele)
+
+#     if len(a) > 0:
+#         print("even")
+#     else:
+#         print("add")
+
+
+# # Driver code
+# print("For List 1:")
+# contains_even_number([1, 9, 8])
+# print("For List 2:")
+# contains_even_number([1, 3, 5])
+
+for letter in 'geeksforgeeks':
+  if letter == 'e' or letter == 's':
+   
+     print(letter)
