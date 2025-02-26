c909c3ba8844e234f333458597ea6cc1ee45bba5
Sankar <sankarkarunakaran1109@gmail.com>
2025-02-26 16:36:09 +0530

    python basics learning

--- 07.02.25/class and objects.py
+++ 07.02.25/class and objects.py
@@ -1,4 +1,6 @@
 class students:
+    total1=500 # static for all students
+
     def __init__(self,name,tamil,eng,math,sci,sco): #constructor
         self.Name=name
         self.Tamil=tamil
@@ -8,14 +10,18 @@ class students:
         self.Social=sco
 
     def total(self):
-        total=500  # static for all students
+         
         add=self.Tamil+self.English+self.Maths+self.Science+self.Social
-        print(f'{self.Name} total mark is {add}/{total}')
+
+        if add>400:
+            print("You were passed in A class ",add)
+            print(f'{self.Name} total mark is {add}/{students.total1}')
 
 
 student1=students("sankar",90,99,89,88,92)
 
 student1.total()
+print("All the students should get 400 out of",students.total1)
 
 
   
---
+++ 26.02.25/practice.py
@@ -0,0 +1,34 @@
+# #amout that jam have
+# amount=50
+# #the price of the item is 15
+# price=15
+# #tax for that item is 3%
+# tax_amount=0.45
+# tax=price*tax_amount
+# remaining=amount-price
+# #the amount after the tax   
+# remaining=remaining-(tax)
+# print(remaining)
+
+
+# string ASSIGNMENT
+
+#numbers of days to birthday print the remaining days by week
+
+# days =input("Enter the number of days to your birthday: ")
+# weeks = int(days)//7
+# remaining_days = int(days) % 7
+# print(f"{weeks} weeks and {remaining_days} days to your birthday")
+
+
+# print the list of animals in the zoo
+
+animals = ["lion", "tiger", "elephant", "giraffe", "zebra"]
+print(animals)
+animals.pop(3)
+print(animals)
+animals.append("monkey")
+print(animals)
+animals.pop(0)
+print(animals)
+print(animals[0:3])
